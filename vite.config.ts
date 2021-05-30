494ffd691faf401471b02cd9bbad3add9f808abc
Simone Romano <simoneromano@tutanota.de>
2021-05-30 20:09:52 +0100

    add BASE_URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: process.env.BASE_URL || "/",
+  plugins: [vue()],
 })
