3e8f019eedb32c4f2ff3e9efbcbb8c46f5134494
Simone Romano <simoneromano@tutanota.de>
2021-06-28 21:13:22 +0200

    materialize-css until vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import "materialize-css/dist/css/materialize.css"
+import "materialize-css/dist/js/materialize.js"
+import "@mdi/font/css/materialdesignicons.css"
+
 import App from './App.vue'
 import router from "./routes"
 
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,9 +1,27 @@
 <template>
   <h1>Login</h1>
   <form @submit="sendLogin">
-    <input type="text" name="username" placeholder="username" v-model="username"> <br>
-    <input type="text" name="password" placeholder="password" v-model="password"> <br>
-    <input type="submit" value="Ok">
+    <div class="row">
+      <div class="input-field col s12">
+        <span class="mdi mdi-email prefix"></span>
+        <input id="email" type="email" name="email" v-model="email" class="validate" required>
+        <label for="email">E-Mail</label>
+        <span class="helper-text" data-error="Invalid email!">Your registered e-mail</span>
+      </div>
+    </div>
+    <div class="row">
+      <div class="input-field col s12">
+        <span class="mdi mdi-key prefix"></span>
+        <input id="password" type="password" name="password" v-model="password" class="validate" required>
+        <label for="password">Password</label>
+        <span class="helper-text" data-error="Invalid password!">A secure password or passphrase</span>
+      </div>
+    </div>
+
+    <button class="btn waves-effect waves-light" type="submit">
+      Login
+      <i class="mdi mdi-send right"></i>
+    </button>
   </form>
 </template>
 
@@ -17,14 +35,14 @@ import httpClient from "../utils/httpClient"
 const LoginPage = defineComponent({
   setup(props, context) {
     const route = useRoute()
-    const username = ref("")
+    const email = ref("")
     const password = ref("")
 
     const loginChallenge = computed(() => route.query.login_challenge as string)
 
     const sendLogin = async (e: Event) => {
       e.preventDefault()
-      const json = {username: username.value, password: password.value, }
+      const json = {email: email.value, password: password.value, }
       try {
         const response: { redirect_to?: string } = await httpClient.post("v1/login", {json, searchParams: { "login_challenge": loginChallenge.value } }).json()
         console.log(response);
@@ -44,8 +62,8 @@ const LoginPage = defineComponent({
     return {
       loginChallenge,
       sendLogin,
-      username,
-      password
+      email,
+      password,
     }
   },
 })
