778c1c5426e8d4ced7f61c696e964f2d30a93ff0
Simone Romano <simoneromano@tutanota.de>
2021-06-29 16:39:02 +0200

    wip

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,56 +1,74 @@
 <template>
-  <h1>Signup</h1>
-  <form @submit="sendLogin">
-    <div class="row">
-      <div class="input-field col s12">
-        <i class="material-icons prefix mdi mdi-email" />
-        <input id="email" v-model="email" type="email" name="email" class="validate" required />
-        <label for="email">
-          E-Mail
-          <code>*</code>
-        </label>
-        <span class="helper-text" data-error="Invalid or empty email!">
-          Your preferred e-mail, check that it is correct since it will be validated! We will send a verification code
-        </span>
-      </div>
-    </div>
-    <div class="row">
-      <div class="input-field col s12">
-        <i class="material-icons prefix mdi mdi-account" />
-        <input id="username" v-model="username" type="text" name="username" />
-        <label for="username">Username</label>
-        <span class="helper-text">An optional username</span>
-      </div>
-    </div>
+  <div class="container">
+    <h1>Signup</h1>
+    <form @submit="sendLogin">
+      <div class="card">
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix mdi mdi-email" />
+            <input id="email" v-model="email" type="email" name="email" class="validate" required />
+            <label for="email">
+              E-Mail
+              <code>*</code>
+            </label>
+            <span
+              class="helper-text"
+              data-error="Invalid or empty email!"
+            >Your preferred e-mail, check that it is correct since it will be validated! We will send a verification code</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix mdi mdi-account" />
+            <input id="username" v-model="username" type="text" name="username" />
+            <label for="username">Username</label>
+            <span class="helper-text">An optional username</span>
+          </div>
+        </div>
 
-    <div class="row">
-      <div class="input-field col s12">
-        <i class="material-icons prefix mdi mdi-key" />
-        <input id="password" v-model="password" type="password" name="password" class="validate" required />
-        <label for="password">
-          Password
-          <code>*</code>
-        </label>
-        <span class="helper-text" data-error="Invalid or empty password!">
-          A secure password or passphrase that you will use to access your account
-        </span>
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix mdi mdi-key" />
+            <input
+              id="password"
+              v-model="password"
+              type="password"
+              name="password"
+              class="validate"
+              required
+            />
+            <label for="password">
+              Password
+              <code>*</code>
+            </label>
+            <span
+              class="helper-text"
+              data-error="Invalid or empty password!"
+            >A secure password or passphrase that you will use to access your account</span>
+          </div>
+        </div>
+
+        <div class="row">
+          The fields with
+          <code>*</code> means that they are required!
+        </div>
+
+        <div class="card-action actions">
+          <button class="btn-flat waves-effect red-text" type="reset">
+            Cancel
+          </button>
+          <button class="btn waves-effect waves-light" type="submit">
+            Signup
+            <i class="material-icons right mdi mdi-send"></i>
+          </button>
+        </div>
       </div>
-    </div>
-
-    <div class="row">
-      The fields with
-      <code>*</code> means that they are required!
-    </div>
-
-    <button class="btn waves-effect waves-light" type="submit">
-      Signup
-      <i class="material-icons right mdi mdi-send"></i>
-    </button>
-  </form>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from "vue"
+import { computed, defineComponent, reactive, ref } from "vue"
 import { useRoute } from "vue-router"
 import { HTTPError } from "ky"
 
@@ -58,13 +76,17 @@ import httpClient from "../utils/httpClient"
 
 const SignupPage = defineComponent({
   setup(props, context) {
-    const email = ref("")
-    const password = ref("")
-    const username = ref()
+    const signupState = reactive({
+      email: null,
+      password: null,
+      username: null,
+    })
 
     const sendLogin = async (e: Event) => {
       e.preventDefault()
-      const json = { email: email.value, password: password.value, username: username.value }
+      const { email, username, password } = signupState
+      // When username is empty, send null
+      const json = { email, password, username: username || null }
       try {
         const response: { redirect_to?: string } = await httpClient.post("v1/signup", { json }).json()
         console.log(response)
@@ -83,11 +105,17 @@ const SignupPage = defineComponent({
 
     return {
       sendLogin,
-      username,
-      email,
-      password,
+      username: signupState.username,
+      email: signupState.email,
+      password: signupState.password,
     }
   },
 })
 export default SignupPage
 </script>
+
+<style lang="sass">
+.actions
+  display: flex
+  justify-content: flex-end
+</style>
